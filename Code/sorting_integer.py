3ef12521163124f1dfbbf0ce9cd5aaacc1856f0b
Wenzel Lowe <brotherwenzel@gmail.com>
2020-04-14 14:41:56 -0700

    add count sort

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -6,6 +6,29 @@ def counting_sort(numbers):
     then looping over counts and copying that many numbers into output list.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
+    min_int = 0
+    max_int = 0
+    for val in numbers:
+        if val >= max_int:
+            max_int = val
+        if val <= min_int:
+            min_int = val
+
+    counts = [None] * ((max_int+1) - min_int)
+
+    for val in numbers:
+        if counts[val] is not None:
+            counts[val] += 1
+        else:
+            counts[val] = 1
+
+    result = []
+    for idx, val in enumerate(counts):
+        if val is not None:
+            result.extend([idx] * val)
+
+    return result
+
     # TODO: Find range of given numbers (minimum and maximum integer values)
     # TODO: Create list of counts with a slot for each number in input range
     # TODO: Loop over given numbers and increment each number's count
@@ -24,3 +47,9 @@ def bucket_sort(numbers, num_buckets=10):
     # TODO: Sort each bucket using any sorting algorithm (recursive or another)
     # TODO: Loop over buckets and append each bucket's numbers into output list
     # FIXME: Improve this to mutate input instead of creating new output list
+
+
+if __name__ == "__main__":
+    arr = [6, 2, 8, 3, 2, 9, 2, 10, 5, 7, 1, 6]
+
+    print(counting_sort(arr))

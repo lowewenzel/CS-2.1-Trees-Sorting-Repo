3534dcedf2f40f8361ad9d3da132fcf8f118c9d3
Wenzel Lowe <brotherwenzel@gmail.com>
2020-04-14 14:24:11 -0700

    add quicksort

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -6,6 +6,11 @@ def is_sorted(items):
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Check that all adjacent items are in order, return early if so
+    length = len(items)
+    for i in range(length - 1):
+        if items[i] > items[i+1]:
+            return False
+    return True
 
 
 def bubble_sort(items):
@@ -13,8 +18,17 @@ def bubble_sort(items):
     repeating until all items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Swap adjacent items that are out of order
+    if is_sorted(items):
+        return True
+    sorted = False
+    while not sorted:
+        swapped = False
+        for i in range(len(items)-1):
+            if items[i] > items[i+1]:
+                items[i], items[i+1] = items[i+1], items[i]
+                swapped = True
+        if not swapped:
+            sorted = True
 
 
 def selection_sort(items):
@@ -22,6 +36,13 @@ def selection_sort(items):
     unsorted item, and repeating until all items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
+    for i in range(len(items)):
+        mindex = i
+
+        for j in range(i + 1, len(items)):
+            if items[j] < items[mindex]:
+                mindex = j
+        items[mindex], items[i] = items[i], items[mindex]
     # TODO: Repeat until all items are in sorted order
     # TODO: Find minimum item in unsorted items
     # TODO: Swap it with first unsorted item
@@ -32,6 +53,15 @@ def insertion_sort(items):
     order in front of items, and repeating until all items are in order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
+    for i in range(1, len(items)):
+        val = items[i]
+        maxdex = i
+
+        while maxdex and items[maxdex - 1] > val:
+            items[maxdex] = items[maxdex - 1]
+            maxdex = maxdex - 1
+        items[maxdex] = val
+
     # TODO: Repeat until all items are in sorted order
     # TODO: Take first unsorted item
     # TODO: Insert it in sorted order in front of items
--- Code/sorting_recursive.py
+++ Code/sorting_recursive.py
@@ -1,4 +1,5 @@
 #!python
+from sorting_iterative import bubble_sort
 
 
 def merge(items1, items2):
@@ -6,9 +7,36 @@ def merge(items1, items2):
     and return a new list containing all items in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
+
     # TODO: Repeat until one list is empty
     # TODO: Find minimum item in both lists and append it to new list
     # TODO: Append remaining items in non-empty list to new list
+    if not items1 and not items2:
+        return []
+
+    result = []
+    i, j = 0, 0
+    length1, length2 = len(items1), len(items2)
+
+    while (i < length1) and (j < length2):
+        if items1[i] <= items2[j]:
+            result.append(items1[i])
+            i += 1
+        else:
+            result.append(items2[j])
+            j += 1
+
+    if i <= length1-1:
+        while i < length1:
+            result.append(items1[i])
+            i += 1
+
+    elif j <= length2-1:
+        while j < length2:
+            result.append(items2[j])
+            j += 1
+
+    return result
 
 
 def split_sort_merge(items):
@@ -21,6 +49,14 @@ def split_sort_merge(items):
     # TODO: Sort each half using any other sorting algorithm
     # TODO: Merge sorted halves into one list in sorted order
 
+    middle = len(items) // 2
+    arr1, arr2 = items[0:middle], items[middle:]
+
+    bubble_sort(arr1)
+    bubble_sort(arr2)
+
+    return merge(arr1, arr2)
+
 
 def merge_sort(items):
     """Sort given items by splitting list into two approximately equal halves,
@@ -31,6 +67,19 @@ def merge_sort(items):
     # TODO: Split items list into approximately equal halves
     # TODO: Sort each half by recursively calling merge sort
     # TODO: Merge sorted halves into one list in sorted order
+    if len(items) <= 1:
+        return items  # base
+
+    mid = len(items) // 2
+    arr1, arr2 = items[0:mid], items[mid:]
+
+    merge_sort(arr1)
+    merge_sort(arr2)
+
+    merged_items = merge(arr1, arr2)
+    # copies the sorted part each time
+    items[:] = merged_items
+    return items
 
 
 def partition(items, low, high):
@@ -45,6 +94,17 @@ def partition(items, low, high):
     # TODO: Move items less than pivot into front of range [low...p-1]
     # TODO: Move items greater than pivot into back of range [p+1...high]
     # TODO: Move pivot item into final position [p] and return index p
+    pivot = items[high]  # just use the end
+
+    cursor = low  # cursor is where the comparisons and swaps occur
+    for i in range(low, high):
+        if items[i] <= pivot:
+            items[i], items[cursor] = items[cursor], items[i]
+            cursor += 1
+
+    items[high], items[cursor] = items[cursor], items[high]
+
+    return cursor
 
 
 def quick_sort(items, low=None, high=None):
@@ -57,3 +117,20 @@ def quick_sort(items, low=None, high=None):
     # TODO: Check if list or range is so small it's already sorted (base case)
     # TODO: Partition items in-place around a pivot and get index of pivot
     # TODO: Sort each sublist range by recursively calling quick sort
+    if low is None:
+        low = 0
+    if high is None:
+        high = len(items) - 1
+
+    if low <= high:
+        p = partition(items, low, high)
+        quick_sort(items, low, p - 1)  # left sublist
+        quick_sort(items, p + 1, high)  # right sublist
+
+
+if __name__ == "__main__":
+    arr = [33, 11, 84, 55, 77, 99, 40, 41, 42]
+    print(arr)
+    piv = partition(arr, 0, len(arr) - 1)
+    print("pivot: ", arr[piv])
+    print(arr)

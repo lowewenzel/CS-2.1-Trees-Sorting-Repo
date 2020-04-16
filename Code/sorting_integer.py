48eb08b4c42d3c4c0d5d207b0e1cd647ea89fce1
Wenzel Lowe <brotherwenzel@gmail.com>
2020-04-16 15:00:55 -0700

    Add bucket sort

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -14,7 +14,7 @@ def counting_sort(numbers):
         if val <= min_int:
             min_int = val
 
-    counts = [None] * ((max_int+1) - min_int)
+    counts = [None] * ((max_int + 1) - min_int)
 
     for val in numbers:
         if counts[val] is not None:
@@ -30,8 +30,8 @@ def counting_sort(numbers):
     return result
 
     # TODO: Find range of given numbers (minimum and maximum integer values)
-    # TODO: Create list of counts with a slot for each number in input range
     # TODO: Loop over given numbers and increment each number's count
+    # TODO: Create list of counts with a slot for each number in input range
     # TODO: Loop over counts and append that many numbers into output list
     # FIXME: Improve this to mutate input instead of creating new output list
 
@@ -41,6 +41,26 @@ def bucket_sort(numbers, num_buckets=10):
     then sorting each bucket and concatenating all buckets in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
+    # O(n)
+    max_num = max(numbers)
+
+    # O(b)
+    buckets = [[] for _ in range(num_buckets)]
+
+    # O(n)
+    for num in numbers:
+        bucket_index = (num * num_buckets) // (max_num + 1)
+        buckets[bucket_index].append(num)
+
+    res = []
+
+    # O(b * n log n)
+    for bucket in buckets:
+        # O(n log n)
+        res.extend(bucket.sort())
+
+    return res
+
     # TODO: Find range of given numbers (minimum and maximum values)
     # TODO: Create list of buckets to store numbers in subranges of input range
     # TODO: Loop over given numbers and place each item in appropriate bucket
@@ -50,6 +70,6 @@ def bucket_sort(numbers, num_buckets=10):
 
 
 if __name__ == "__main__":
-    arr = [6, 2, 8, 3, 2, 9, 2, 10, 5, 7, 1, 6]
+    arr = [6, 2, 8, 3, 2, 9, 2, 10, 5, 7, 1, 6, 7, 9999]
 
-    print(counting_sort(arr))
+    print(bucket_sort(arr))
